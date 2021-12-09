07f33671585272bb578903e2fbd04f410d59d76e
howlingsails <cap10bill@howlingsails.com>
2021-12-09 07:37:10 -0800

    Adding more submodules to help in our quest.

--- addSubModules.sh
+++ addSubModules.sh
@@ -4,4 +4,12 @@
 git submodule add git@github.com:howlingsails/Fantasy-Map-Generator.git
 git submodule add git@github.com:howlingsails/dnd5e-town-generator.git
 git submodule add git@github.com:howlingsails/opendnd.git
-
+git submodule add git@github.com:opendnd/genetica.git
+git submodule add git@github.com:opendnd/dominia.git
+git submodule add git@github.com:opendnd/dynastia.git
+git submodule add git@github.com:opendnd/nomina.git
+git submodule add git@github.com:opendnd/personae.git
+git submodule add git@github.com:opendnd/similia.git
+git submodule add git@github.com:opendnd/modules.git
+git submodule add git@github.com:opendnd/questae.git
+git submodule add git@github.com:opendnd/compendia.git
